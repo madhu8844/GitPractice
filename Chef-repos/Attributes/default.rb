f4bfd22f65a40e50fdfec0461173ffb55c9ac113
madhu8844 <madhu.8844@gmail.com>
2016-10-26 05:09:39 -0700

    Chefrepo

---
+++ Chef-repos/Attributes/default.rb
@@ -0,0 +1,1 @@
+default["mypackage"]["name"] = 'ngnix'
---
+++ Chef-repos/recipes/default.rb
@@ -0,0 +1,14 @@
+mypackage = node["mypackage"]["name"]
+package mypackage do
+action:install
+end
+
+
+#
+# Cookbook Name:: Yummy
+# Recipe:: default
+#
+# Copyright 2016, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
